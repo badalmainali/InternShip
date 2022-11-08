24660d755b7025724c0fb791864d61266d777703
badalmainali <badalma12@gmail.com>
2022-11-08 22:24:05 +0545

    react

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 
 import './App.css';
 import Navbars from './Component/Navbar';
+import {BrowserRouter,Routes,Route} from 'react-router-dom';
+import Registerpage from './Screens/Register';
 function App() {
   return (
     <div className="App">
       <Navbars />
+      <BrowserRouter>
+      <Routes>
+        <Route path='/register' exact element={<Registerpage />} />
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
